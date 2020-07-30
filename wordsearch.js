3de60e64447a265d7a6939be39c44a6ac8990d3c
Caia Chuang <caiachung@gmail.com>
2020-07-30 01:32:25 +0000

    pair programming with Grace

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = flipToVerticalArray(letters).map(vs => vs.join(''));
+    let isMatch = false;
+    
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+      if (l.includes(word) ) {
+        isMatch = true;
+        return true;
+      } 
+    };
+    if(!isMatch){
+      for ( let v of verticalJoin ) {
+        if (v.includes(word)){
+          return true;
+        }
+      }
+    };
+    return false
+};
+
+const flipToVerticalArray = function(array) {
+  let verticalArray = [];
+  let len = array.length;
+  let vlen = array[0].length;
+  
+  for(let i = 0; i < vlen; i++){
+    verticalArray[i] = []
+    for(let j = 0; j < len ; j++) {
+      verticalArray[i][j] = array[j][i]
     }
-}
+  };
+  return verticalArray;
+};
 
 module.exports = wordSearch
+
